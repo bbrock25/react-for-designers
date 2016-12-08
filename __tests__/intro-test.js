b8f84eeb1e6bf054f33d13b5363db4c4f3561902
Bill Brock <william.e.brock@gmail.com>
2016-12-07 21:56:46 -0500

    removing all but the readme

--- __tests__/intro-test.js
+++
@@ -1,7 +0,0 @@
-import * as intro from '../intro'
-
-context('Welcome!', () => {
-  test('the intro message should match what we are expecting', () => {
-    expect(intro.message).toEqual('fix this test and proceed to lesson-1')
-  })
-})
